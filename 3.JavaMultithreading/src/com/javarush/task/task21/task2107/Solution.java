f6265e4896bdbe5553d43d0e66c272a2e0a9d905
Slava <_fish_@ukr.net>
2022-11-05 21:39:23 +0200

    JR 5.11.22

---
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2107/Solution.java
@@ -0,0 +1,82 @@
+package com.javarush.task.task21.task2107;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Objects;
+
+/* 
+Глубокое клонирование карты
+*/
+
+public class Solution implements Cloneable {
+
+    public static void main(String[] args) throws CloneNotSupportedException {
+        Solution solution = new Solution();
+        solution.users.put("Hubert", new User(172, "Hubert"));
+        solution.users.put("Zapp", new User(41, "Zapp"));
+        Solution clone = null;
+        clone = solution.clone();
+        System.out.println(solution);
+        System.out.println(clone);
+
+        System.out.println(solution.users);
+        System.out.println(clone.users);
+
+    }
+
+    public Solution(Map<String, User> users) {
+        this.users = users;
+    }
+
+    public Solution() {
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Solution)) return false;
+        Solution solution = (Solution) o;
+        return Objects.equals(users, solution.users);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(users);
+    }
+
+    protected Map<String, User> users = new LinkedHashMap();
+    protected Solution clone() throws CloneNotSupportedException {
+        Solution clone = (Solution) super.clone();
+        clone.users = new LinkedHashMap<>();
+        for (Map.Entry<String, User> entry : users.entrySet()) {
+            clone.users.put(entry.getKey(), entry.getValue().clone());
+        }
+        return clone;
+    }
+    public static class User implements Cloneable {
+        int age;
+        String name;
+
+        public User(int age, String name) {
+            this.age = age;
+            this.name = name;
+        }
+
+        public User clone() throws CloneNotSupportedException {
+            return (User) super.clone();
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (!(o instanceof User)) return false;
+            User user = (User) o;
+            return age == user.age && Objects.equals(name, user.name);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(age, name);
+        }
+    }
+}
